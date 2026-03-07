1a2ce36ba90637a2dc67fa42fde7e26fff4ccbc6
Alexandre Sanchez <alex73630@gmail.com>
2026-03-08 00:06:42 +0100

    feat: setup tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://alex73630.xyz",
-	integrations: [react(), sitemap()]
+	integrations: [react(), sitemap()],
+
+	vite: {
+		plugins: [tailwindcss()]
+	}
 })
