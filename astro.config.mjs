f3970ea7aae1fa3544d771858d35fbba488627a8
Alexandre Sanchez <alex73630@gmail.com>
2026-03-07 22:18:41 +0100

    chore: setup linting and formatting with oxc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()]
-});
+	integrations: [react(), sitemap()]
+})
